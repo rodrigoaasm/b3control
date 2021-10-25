8fa61ae546cae428772bfbcc3de46f86fa82e4a1
Rodrigo Maia <rodrigoasmaia@gmail.com>
2021-10-24 21:32:38 -0300

    [API] Fix: Lack of asset quote model indexing

--- API/src/external/datasource/relational/models/index.ts
+++ API/src/external/datasource/relational/models/index.ts
@@ -1,14 +1,17 @@
 import { OperationModel } from './operation-model';
 import { AssetModel } from './asset-model';
+import { AssetQuoteModel } from './asset-quote-model';
 
 const persistentEntities = [
   OperationModel,
   AssetModel,
+  AssetQuoteModel,
 ];
 
 export {
   OperationModel,
   AssetModel,
+  AssetQuoteModel,
 };
 
 export default persistentEntities;
